cda783a0bda029ef8fed50d60683422f6828169c
villafrade <cavillafrades@gmail.com>
2022-03-07 08:16:05 -0500

    taller listo

--- mediana.js
+++ mediana.js
@@ -1,4 +1,22 @@
-function mediana(lista)
+var lista=[];
+var mostrarLista = document.getElementById("respuesta_lista");
+var respuestaMediana = document.getElementById("respuesta_mediana");
+
+function agregar_numero()
+{
+var p = document.getElementById("input_lista");
+var numero_agregar = parseInt(p.value);
+lista.push( numero_agregar);
+mostrarLista.innerHTML = ("tu lista es: {" + lista + "}");
+}
+
+function borrar_numero()
+{
+    lista.pop();
+    mostrarLista.innerHTML = ("tu lista es: {" + lista + "}");
+}
+
+function calcular_mediana()
 {
     const cantidad_lista = lista.length;
     const mediana_lista = Math.floor((lista.length / 2));
@@ -6,11 +24,11 @@ function mediana(lista)
     {
         const a = mediana_lista - 1;
         const resultado_mediana = (lista[mediana_lista] + lista[a]) / 2;
-        console.log("la media de la lista es: " + resultado_mediana);
+        respuestaMediana.innerHTML=("la media de la lista es: " + resultado_mediana);
         
     }
     else
     {
-        console.log("la mediana de la lista es: " + lista[mediana_lista] );
+        respuestaMediana.innerHTML=("la mediana de la lista es: " + lista[mediana_lista] );
     }
 }
